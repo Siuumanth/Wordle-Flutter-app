1a849024a5b520a563209545709979416b8fe59d
Siuumanth <gsumanth3005@gmail.com>
2024-10-31 17:57:44 +0530

    cleaned some files, added constants and home page

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color theme = Color.fromARGB(166, 86, 255, 185);
+const Color grey = Color.fromARGB(214, 74, 74, 74);
+const Color grey_lessO = Color.fromARGB(195, 74, 74, 74);
+const Color darktheme = Color(0xff2a697d);
