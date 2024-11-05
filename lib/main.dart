1ac8968a48c52829cfee96fe38524e3a5b10a9e0
Siuumanth <gsumanth3005@gmail.com>
2024-11-05 16:38:32 +0530

    added gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:wordle/screens/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
