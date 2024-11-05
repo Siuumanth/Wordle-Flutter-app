9e6a6eb85d6a8254feeb53475762006c1efef180
Siuumanth <gsumanth3005@gmail.com>
2024-11-06 00:36:13 +0530

    added wrapper

---
+++ lib/wrapper.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:wordle/screens/home.dart';
+import 'package:wordle/screens/login/login.dart';
+
+class Wrapper extends StatefulWidget {
+  const Wrapper({super.key});
+
+  @override
+  State<Wrapper> createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return const Center(child: Text("Error"));
+          } else {
+            if (snapshot.data == null) {
+              //not logged in
+              return const Login();
+            } else {
+              return Home();
+            }
+          }
+        },
+      ),
+    );
+  }
+}
